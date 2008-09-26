b07d4c2e7f122116b3d414a69f3d051f47757225
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2008-09-26 23:36:26 +0000

    add indent block function

--- trunk/scripts/update_header.pl
+++ trunk/scripts/update_header.pl
@@ -109,3 +109,43 @@ sub extract_blocks {
 
   return @blocks;
 }
+
+
+sub indent_block {
+  
+  # indent a function's prototype declaration to opening parenthesis
+  
+  my $block = shift;
+  
+  $block =~ s/^\s*/  /g;
+  $block =~ s/\s*$//g;
+  
+  my $indent_spaces = "";
+  
+  my @lines = split("\n", $block);
+  my $line;
+  
+  my $is_first_line = 1;
+  foreach $line ( @lines ){
+    
+    if ($is_first_line == 1){
+      
+      if ($line !~ /^(.*?\()/){
+	print "Error, can't match start of function\n";
+      }
+      
+      $indent_spaces = " " x length($1);
+      $is_first_line = 0;
+      next;
+    }
+    
+    $line =~ s/^\s*//g;
+    $line = $indent_spaces . $line;
+  }
+
+  $block = join ("\n", @lines);
+  $block = $block . "\n\n";
+
+  return $block;
+
+}
