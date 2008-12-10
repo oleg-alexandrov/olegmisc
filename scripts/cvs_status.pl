1d08cf57635d98f7efe1907fccbd333f496a6430
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2008-12-10 17:09:09 +0000

    new script

---
+++ scripts/cvs_status.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+use strict;		      # 'strict' insists that all variables be declared
+use diagnostics;	      # 'diagnostics' expands the cryptic warnings
+undef $/; # undefines the separator. Can read one whole file in one scalar.
+
+# use main to avoid the curse of global variables
+MAIN: {
+
+  # Clean up cvs status
+  
+  my @lines = split("===", <>);
+
+  my ($line, $file, $status, @files, @stats, $file_len);
+
+  @files = ();
+  @stats = ();
+  $file_len = 0;
+  
+  foreach $line (@lines){
+
+    next if ($line =~ /Status:\s+Up-to-date/i);
+
+    next unless ($line =~ /Status:\s*(.*?)\n/i);
+    $status = $1;
+    
+    next unless ($line =~ /Repository\s+revision:.*?(\/.*),/i);
+    $file = $1;
+
+    $file =~ s/^.*?(dev\/)/$1/g;
+    $file =~ s/Attic\///g;
+
+    push(@files, $file);
+    push(@stats, $status);
+
+    if ($file_len < length($file)){
+      $file_len = length($file);
+    }
+    
+  }
+
+  my $counter = 0;
+
+  foreach $file (@files){
+    $status = $stats[$counter];
+
+    # pad with spaces
+    $file = $file . " " x ($file_len - length($file));
+    
+    print "File: $file $status\n";
+    
+  }
+}
