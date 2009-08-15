6f30043ea17b580cb47650a131016ac9f4e2e3a8
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2009-08-15 00:17:41 +0000

    more

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -6,6 +6,16 @@
 (fset 'yes-or-no-p 'y-or-n-p)  ;; Make all "yes or no" prompts show "y or n" instead
 
 (setq make-backup-files nil) 
+(setq version-control 'never)
+(setq vc-cvs-stay-local nil)
+
+(setq mouse-yank-at-point t)
+
+(setq default-frame-alist
+      '(       
+        (cursor-color . "Red")
+        (cursor-type . bar)
+))
 
 ;;start gnuserv.
 ;(gnuserv-start)(setq make-backup-files nil) 
@@ -133,7 +143,7 @@
 
 
 ;;matlab
-(autoload 'matlab-mode "~/.emacs.d/matlab.elc" "matlab major mode." t)
+(autoload 'matlab-mode "~/.emacs.d/matlab.el" "matlab major mode." t)
 (add-hook 'matlab-mode-hook 
 	  '(lambda()
 	     ;;my personal stuff
@@ -213,6 +223,7 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
+ '(column-number-mode t)
  '(cua-mode t nil (cua-base))
  '(scroll-bar-mode (quote right)))
 (custom-set-faces
@@ -221,20 +232,28 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 123 :width normal :foundry "b&h" :family "Luxi Mono"))))
+ '(bold-italic ((t (:slant italic :weight bold))))
  '(cperl-array-face ((((type x) (class color) (background light dark)) (:foreground "orangered" :bold t))))
  '(cperl-hash-face ((((class color) (background dark)) (:foreground "Red" :background "black" :bold t :italic nil))))
  '(cperl-nonoverridable-face ((((class color) (background dark)) (:foreground "orange" :family "Courier" :bold t :italic nil))))
+ '(cursor ((default (:width condensed)) (nil nil)))
+ '(custom-button ((t (:bold t :foreground "#3fdfcf"))))
  '(custom-button-face ((t (:bold t :foreground "#3fdfcf"))) t)
+ '(custom-face-tag ((t (:bold nil :italic nil :underline t))))
  '(custom-face-tag-face ((t (:bold nil :italic nil :underline t))) t)
+ '(custom-group-tag ((((class color) (background light)) (:underline t :foreground "blue"))))
  '(custom-group-tag-face ((((class color) (background light)) (:underline t :foreground "blue"))) t)
+ '(custom-saved ((t (:underline t :foreground "orange"))))
  '(custom-saved-face ((t (:underline t :foreground "orange"))) t)
+ '(custom-state ((((class color) (background light)) (:foreground "dark green"))))
  '(custom-state-face ((((class color) (background light)) (:foreground "dark green"))) t)
+ '(custom-variable-button ((t (:bold t :underline t :foreground "white"))))
  '(custom-variable-button-face ((t (:bold t :underline t :foreground "white"))) t)
  '(diff-context-face ((((class color) (background light)) (:foreground "yellow"))))
  '(dired-face-boring ((((type x pm mswindows) (class color grayscale) (background light)) (:foreground "red"))))
  '(dired-face-permissions ((t (:foreground "green"))))
- '(flyspell-duplicate-face ((((class color)) (:foreground "OrangeRed"))))
- '(flyspell-incorrect-face ((((class color)) (:foreground "OrangeRed"))))
+ '(flyspell-duplicate-face ((((class color)) (:foreground "OrangeRed"))) t)
+ '(flyspell-incorrect-face ((((class color)) (:foreground "OrangeRed"))) t)
  '(font-latex-bold-face ((((class color) (background light)) (:bold t))))
  '(font-latex-italic-face ((nil (:bold nil :italic nil))))
  '(font-latex-math-face ((t (:bold nil :foreground "green3"))))
@@ -258,27 +277,31 @@
  '(gnus-cite-face-7 ((((class color) (background light)) (:foreground "yellow"))))
  '(gnus-header-content-face ((((class color) (background light)) (:foreground "red" :italic t))))
  '(green ((t (:foreground "green"))) t)
- '(highlight ((t (:foreground "red3" :background "white"))))
+ '(highlight ((t (:foreground "red3"))))
  '(hyper-apropos-documentation ((((class color) (background light)) (:foreground "red"))))
  '(info-node ((t (:bold t))))
- '(isearch ((t (:foreground "red" :background "white"))))
+ '(isearch ((t (:foreground "red"))))
+ '(lazy-highlight ((((class color) (min-colors 88) (background light)) (:foreground "red"))))
  '(list-mode-item-selected ((t (:foreground "green"))) t)
  '(message-cited-text ((t (:foreground "green"))))
  '(message-header-contents ((t (:italic nil))))
  '(message-headers ((t (:foreground "blue" :bold t))))
  '(message-highlighted-header-contents ((t (:bold t))))
- '(message-separator-face ((((class color) (background light)) (:foreground "red"))))
+ '(message-separator-face ((((class color) (background light)) (:foreground "red"))) t)
  '(message-url ((t (:foreground "orange" :bold t))))
  '(mh-show-to-face ((((class color) (background light)) (:foreground "red"))))
+ '(minibuffer-prompt ((t (:foreground "royal blue"))))
  '(mutt-header-keyword-face ((((class color) (background dark)) (:foreground "cyan" :bold nil))))
  '(mutt-header-value-face ((((class color) (background dark)) (:foreground "indianred1"))))
  '(mutt-multiply-quoted-text-face ((((class color) (background dark)) (:foreground "gold" :italic nil))))
  '(mutt-quoted-text-face ((((class color) (background dark)) (:foreground "green" :italic nil))))
+ '(region ((nil (:background "royalblue"))))
  '(right-margin ((t (:bold nil :italic nil))) t)
  '(secondary-selection ((t (:foreground "white" :background "red"))))
  '(text-cursor ((t (:foreground "black" :background "green"))) t)
  '(underline ((t nil)))
  '(viper-minibuffer-insert-face ((((class color)) (:foreground "white" :background "black"))))
+ '(widget-field ((((class grayscale color) (background light)) (:foreground "black" :background "white"))))
  '(widget-field-face ((((class grayscale color) (background light)) (:foreground "black" :background "white"))) t)
  '(x-face ((t (:foreground "gold" :background "black"))))
  '(zmacs-region ((t (:background "RoyalBlue"))) t))
