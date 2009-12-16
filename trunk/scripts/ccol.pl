2cf88e59dacb9473f3cb0fe2debc24535450f185
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2009-12-16 19:40:56 +0000

    add

---
+++ trunk/scripts/ccol.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;	   # insist that all variables be declared
+use diagnostics;   # expand the cryptic warnings
+undef $/;          # read one whole file in one scalar
+
+MAIN:{
+
+  # Change the color of an xg file
+  
+  if (scalar(@ARGV) < 2){
+    print "Usage: $0 new_color file.xg \n";
+    exit(0);
+  }
+
+  my $col   = shift @ARGV;
+  my @files = @ARGV;
+
+  foreach my $file (@files){
+    print "Making the color of $file $col\n";
+
+    open(FILE, "<$file");
+    my $text = <FILE>;
+    close(FILE);
+
+    if ($text !~ /color/i){
+      $text = "color = $col\n" . $text;
+    }else{
+      $text =~ s/color.*?\w+/color = $col/i;
+    }
+    
+    open(FILE, ">$file");
+    print FILE $text;
+    close(FILE);
+  }
+  
+}
