5ebaa80015cb77c66b1c6b98aa198ebff39f5fe4
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2009-12-16 18:40:22 +0000

    more

---
+++ scripts/testAddArgs.cpp
@@ -0,0 +1,52 @@
+// Test code. Test with: addArg.pl myfun "int i // some arg" 5 testAddArgs.cpp
+
+some code
+
+some other fun() 
+  
+void object->myfun(// ------------------- Inputs ----------------------------
+                   
+                   // Part 1
+                   int q, double v, double s,
+
+        int * pp, 
+                   
+                     // Part 3
+                   rr, ps, 
+                   
+                   
+                      // ------------------- Outputs ----------------------------
+                   // Here they are
+                   o1, o2, o3
+                   );
+
+
+void myfun(    int a, // first arg, note the comma in comments
+
+
+
+
+
+                          double b, // second arg,  axUp6Ze
+               
+
+
+                      double c, double d // third and fourth,
+
+               
+              );
+
+
+void myfun(
+
+           
+                 );
+
+
+myfun
+
+text here
+
+my fun)
+
+  
