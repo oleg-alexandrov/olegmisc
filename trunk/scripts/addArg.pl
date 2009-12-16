22d5aa9b973d278aaa2cbeabbce3a8964673251f
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2009-12-16 18:40:22 +0000

    more

--- trunk/scripts/addArg.pl
+++ trunk/scripts/addArg.pl
@@ -8,7 +8,17 @@ require 'splitByMatchingParen.pl';
 undef $/;          # read one whole file in one scalar
 
 MAIN:{
-  #addArg.pl myfun "int i // some arg" 5 test.cpp
+
+  # Insert at prescribed position another argument to a function construct like
+  # void myfun(int a,             // first arg, note the comma in comments
+  #            double b,          // second arg,  axUp6Ze 
+  #            double c, double d // third and fourth, 
+  #            );
+
+  # Position starts from 1, not from 0.
+
+  # Test with addArg.pl myfun "int i // some arg" 5 testAddArgs.cpp
+  
   if (scalar(@ARGV) < 4){
     print "Usage: $0 functionName argToAdd argPos files\n";
   }
@@ -19,7 +29,9 @@ MAIN:{
   my @files    = @ARGV;
 
   my $tag = 'xu6Ao u9 '; # something unlikely
-  
+
+  # Visit each file, get the chunk of text starting with $fun,
+  # and insert the new argument at the prescribed location.
   foreach my $file (@files){
 
     open(FILE, "<$file"); my $text = <FILE>; close(FILE);
@@ -31,14 +43,14 @@ MAIN:{
 
       next unless ($block =~ /^.*?\Q$fun\E\s*\(/);
       
-      print "------------------------\n";
-      print "block is\n'$block'\n\n";
+      #print "------------------------\n";
+      #print "block is\n'$block'\n\n";
       #print "Insert at pos $argPos\n";
       
       $block = addArg($block, $argToAdd, $argPos);
       
       #print "\nblock is\n'$block'\n";
-      print "------------------------\n";
+      #print "------------------------\n";
       
     }
 
@@ -46,53 +58,53 @@ MAIN:{
     open(FILE, ">", $file . "_out"); print FILE $text; close(FILE);
     
   }
-
   
 }
 
 sub addArg{
-    
-  # Insert at prescribed position another argument to a function construct like
-  # void myfun(int a, // first arg, note the comma in comments
-  #               double b, // second arg,  axUp6Ze 
-  #               double c, double d // third and fourth, 
-  #               );';
 
-  # Position starts from 1, not from 0.
-
+  # Add $argToAdd at postion $argPos for function $fun.
+  
   my $fun      = shift;
   my $argToAdd = shift;
   my $argPos   = shift;
 
   if ($argPos <= 0){
-    print "Expecting argument position to be >= 1\n";
-    exit(0);
+    $argPos = 1; # We start from 1
   }
   
-  my $comma = ' axUp6Ze '; # something unlikely to encode the comma to
-  while ($fun =~ /$comma/){
-    $comma .= 'a';         # make sure the funny text above does not indeed occur 
-  }
-
+  # Split void 'myfun(int a);' into 'void myfun(', 'int a', ');'
   my ($before, $argList, $after) = splitByMatchingParen($fun);
 
   if ($argList eq "" and $after eq ""){
+    # Could not split properly
     return $before;
   }
+
+  # Strip leading space from each line (will put it back later when indenting)
+  $argList =~ s/(^|\n)[ ]*([^\s])/$1$2/g;
   
   # New arg will be on its on line
   $argToAdd .= "\n";
   $argToAdd =~ s/^\s*//g;
   $argToAdd = "\n" . $argToAdd;
   
-  # Encode any comma in comments as that can be confusing
-  $argList  =~ s/(\/\/.*?[\n]+)/encodeComma($1, $comma)/eg;
-  $argToAdd =~ s/(\/\/.*?[\n]+)/encodeComma($1, $comma)/eg;
+  # Encode any comma in comments to not confuse such comma with the comma
+  # separating function arguments
+  my $comma = ' axUp6Ze '; # something unlikely to encode the comma to
+  while ($fun =~ /$comma/){
+    $comma .= 'a';         # make sure the funny text above does not indeed occur 
+  }
+  $argList  =~ s/(\/\/.*?\n)/encodeComma($1, $comma)/eg;
+  $argToAdd =~ s/(\/\/.*?\n)/encodeComma($1, $comma)/eg;
 
-  # Move comma to the end of line (we want to
-  # group an argument with its comment if any)
-  $argList =~ s/(,)(\s*\/\/.*?\n)/$2$1/g;
-
+  # Strip comma in $argToAdd if any
+  $argToAdd =~ s/,//g;
+  
+  # We want to group an argument with its trailing comment and newlines if any
+  $argList =~ s/(,)(\s*\/\/.*?\n)/$2$1/g; # move comma beyond the comment
+  $argList =~ s/(,)(\s*)/$2$1/g;          # move comma beyond all newlines
+  
   my @args = split(",", $argList);
   my $len  = scalar (@args);
 
@@ -104,10 +116,7 @@ sub addArg{
       push(@outargs, $argToAdd);
     }
 
-    #$args[$i] =~ s/^[ \t]*\n\s*//g; # rm extra newlines
-    #$args[$i] =~ s/\n\s*$/\n/g;     # rm extra newlines
     push(@outargs, $args[$i]);
-    
   }
 
   # If to insert after all other arguments
@@ -115,10 +124,11 @@ sub addArg{
     push(@outargs, $argToAdd);
   }
 
+  # Put back together the arguments
   $argList = join(",", @outargs);
 
-  # Move comma back and decode the comma in comments
-  $argList =~ s/([ \t\r]*[\n]+[ \t\r]*)(,)/$2$1/g;
+  # Move the comma back and decode any comma within comments
+  $argList =~ s/(\s*)(,)/$2$1/g;
   $argList =~ s/(\s*\/\/.*?)(,)/$2$1/g;
   $argList =~ s/$comma/,/g;
   
@@ -135,14 +145,16 @@ sub addArg{
 
   $argList = join("\n", @outargs);
   
-  # Rm excessive spaces, put space at the end
-  $argList  =~ s/^\s*//g;
-  $argList  =~ s/\n[ \t\r]+\n/\n/g;
+  # Put just one newline at the end
+  $argList =~ s/^\s*//g;
   $argList  =~ s/\s*$//g;
   $argList .=  "\n";
 
   # The case of just one argument
   $argList =~ s/^\s*,\s*//g;
+
+  # Strip extra spaces
+  $argList =~ s/[ \t]*\n/\n/g;
   
   $after = $indentLevel . $after;
   $fun = $before . $argList . $after;
