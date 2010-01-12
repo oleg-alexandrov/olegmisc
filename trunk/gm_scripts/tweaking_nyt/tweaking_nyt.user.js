cd1e2706cc6e66bfb6a7e671e61766b0e0212945
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-01-12 02:09:17 +0000

    minor

---
+++ trunk/gm_scripts/tweaking_nyt/tweaking_nyt.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name           Tweaking NYT
+// @namespace      NYT
+// @description    Tweaking NYT
+// @include        http://www.nytimes.com/
+// ==/UserScript==
+
+	
+(function() {
+
+//	document.getElementById('toolsHome').style["display"] = "none";
+
+//var adBar = document.getElementById('toolsHome');
+
+//if (adBar)  adBar.parentNode.removeChild(adBar);
+
+
+//	document.getElementById('item_ad').style["display"] = "none";
+//	document.getElementById('comments_ad').style["display"] = "none";
+//	document.getElementById('col-promo').style["display"] = "none";
+//      document.getElementById('div#topads').style.display="none";
+//      document.getElementById('col-promo').style.display="none";
+
+//var adBar = document.getElementById('col-promo');
+//
+//if (adBar)
+//	adBar.parentNode.removeChild(adBar);
+
+})()
+
+
