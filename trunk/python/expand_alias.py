c6ae755f2190bbf9c2461496b060d2fe9eea623b
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-01-26 02:24:28 +0000

    more

---
+++ trunk/python/expand_alias.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys
+import os
+import re # perl-style regular expressions
+
+numArgs   = len(sys.argv)
+aliasName = sys.argv[numArgs-1]
+
+HOME     = os.getenv("HOME")
+fileName = HOME + '/.bash_aliases'
+
+FILE = open(fileName, "r")
+data = FILE.read()
+FILE.close()
+
+lines = data.split("\n")
+
+out = ""
+
+for line in lines:
+
+    matches = re.match('^(alias ' + aliasName + '.*?)$', line); # raw match
+    if matches:
+        out = matches.group(1)
+
+if out == "":
+    out = aliasName
+
+print out
+    
