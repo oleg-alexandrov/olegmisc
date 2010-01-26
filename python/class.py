83fa4e1f6a921eb3597ef924abf14e6b6c227c1a
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-01-26 02:24:28 +0000

    more

---
+++ python/class.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+class drawnData:
+    
+    drawnLine  = "5"
+    drawnSpace = "6"
+    drawnCd    = "7"
+
+    def __init__(self):
+        print "Calling the init function"
+        self.drawnLine   = -1
+        self.drawnSpace  = -3
+        self.drawnCd     = -5
+
+    def disp(self):
+        print self.drawnLine
+        print self.drawnSpace
+        print self.drawnCd
+        
+    
+if __name__ == '__main__':
+
+    fileName = "data.txt"
+    FILE     = open(fileName, "r")
+    data     = FILE.read()
+    FILE.close()
+
+    A = drawnData()
+    A.disp()
+
+    print "Assigning to the class"
+    
+    A.drawnLine  = 13
+    A.drawnSpace = 14
+    A.disp()
+
+    A.l = 12
+    print "New value is ", A.l
+    
