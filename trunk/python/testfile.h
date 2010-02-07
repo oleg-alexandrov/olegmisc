f53e8529a1e4ff9be76335d8c6cc03a5417391d8
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-02-07 01:31:34 +0000

    minor

---
+++ trunk/python/testfile.h
@@ -0,0 +1,19 @@
+#ifndef FINE_H
+#define FINE_H
+
+// Some function there
+
+// Some function here
+
+class myname{
+  unsigned int myfun(unsigned int i, unsigned j);
+  unsigned int myfun2(unsigned
+                      k);
+
+}
+  
+};
+
+#endif
+
+//extra stuff
