02bdc0f58aa351d2ea6fba5beebda7df33fee5a8
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-02-26 02:11:15 +0000

    minor

---
+++ python/merge_spreads.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import os
+import re # Perl-style regular expressions
+
+if len(sys.argv) < 2:
+    print "Usage:", sys.argv[0], "file1.csv file2.csv ..."
+    sys.exit(1)
+
+cols         = []
+max_num_rows = 0
+for filename in sys.argv[1:]:
+    
+    handle = open(filename, 'r')
+    text = handle.read()
+    handle.close()
+    lines = text.split("\n")
+    lines = [filename] + lines
+    max_num_rows = max(max_num_rows, len(lines))
+    cols.append(lines)
+
+
+# Print each spreadsheet row
+for count in range(max_num_rows):
+
+    row = ""
+    for ptr in cols:
+        if len(ptr) < count + 1:
+            row = row + ","
+        else:
+            row = row + ptr[count] + ","
+            
+    print row
+    
