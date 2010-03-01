46b2b565a3bae6a67507562d63d4403a16f87ee1
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-03-01 04:33:33 +0000

    pymacs

---
+++ trunk/dotfiles/.xemacs/Pymacs/manglers.py
@@ -0,0 +1,10 @@
+from Pymacs import lisp
+interactions = {}
+
+def break_on_whitespace():
+    start, end = lisp.point(), lisp.mark(True)
+    words = lisp.buffer_substring(start, end).split()
+    lisp.delete_region(start, end)
+    lisp.insert('\n'.join(words))
+
+interactions[break_on_whitespace] = ''
