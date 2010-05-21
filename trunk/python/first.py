53da8577d72ae103161267c8c4a9076b688340c2
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-05-21 16:57:03 +0000

    minor

--- trunk/python/first.py
+++ trunk/python/first.py
@@ -2,42 +2,46 @@
 
 import sys
 
-print "hi!\n"
+print "hi there"
 
 print "input arguments are ", sys.argv
 
 vals = sys.argv;
 
-print "program name is ", vals[0]
-print "input option is ", vals[1]
+if len(vals) > 0:
+    print "program name is ", vals[0]
+    
+if len(vals) > 1:
+    print "input option is ", vals[1]
 
-A = ['pqr', 'stu'];
+A = ['abc', 'def'];
 
 print "The array is ", A
 print "number of elements is ", len(A)
-print "First element is ", A[1]
+print "Elements 0 and 1 are", A[0], A[1]
+
+for x in A:
+    print "Listing the elements of A = ", A, " element is:", x
 
 a = 0;
 while ( a < 10 ):
     print "a is ", a
     a += 1
 
-    if a > 5:
-        print "We went beyond 5"
+    if a > 2:
+        print "We went beyond 2"
     else:
-       print "We are under 5"
+       print "We are under 2"
 
-    if a > 8:
-        print "We are breaking"
+    if a > 3:
+        print "break the loop"
         break
     
-for x in A:
-    print "Listing the elements of A = ", A, " element is ", x
-
     
 def first_fun(n):
 
-    print "number is ", n
+    print "First function: number is ", n
 
+# Call the function we just defined
 first_fun(5)
 
