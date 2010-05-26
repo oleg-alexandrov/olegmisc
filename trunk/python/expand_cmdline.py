df27281ad9c4f5fff56b9a67bca07b61ce860c8b
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-05-26 17:57:37 +0000

    minor

--- trunk/python/expand_cmdline.py
+++ trunk/python/expand_cmdline.py
@@ -5,6 +5,8 @@ import os
 import string
 import re # perl-style regular expressions
 
+# Edit the content of the command line as passed from zsh
+
 def expandAliases(aliasName):
 
    # Given the name of an alias, return the line showing its definition
@@ -35,7 +37,48 @@ def expandAliases(aliasName):
 
 # end expandAliases()
 
+def expandCmdLine(cmdLine, cursor):
+
+   lineLen = len(cmdLine)
+   if cursor < 0:
+      cursor = 0
+   elif (cursor > lineLen):
+      cursor = lineLen
+  
+   before = cmdLine[0:cursor]
+   after  = cmdLine[cursor:lineLen]
+
+   words    = re.split("\s+", before)
+   numWords = len(words)
+
+   if ( numWords == 1 and words[0] == "pl" ) or \
+          ( numWords >= 2 and words[numWords-2] != "a" and words[numWords-1] == "pl" ):
+
+      # Expand the string "pl"
+      words[numWords-1] = "perl -pi -e \"s###g\""
+      cmdLine = " ".join(words) + after
+      cursor  = cursor + 13
+
+   elif ( numWords >= 2 and words[numWords-2] == "for" ):
+
+      # Expand "for var" into a full for loop.
+      var = words[numWords-1]
+      words[numWords-2] = "for ((" + var + " = ; " + var + " < ; " + var + "++)); " + \
+                          "do ; done" 
+      words[numWords-1] = ""
+      cmdLine = " ".join(words) + after
+      cursor  = cursor + 5
+      
+   elif (numWords == 2 and words[0] == "a"):
+
+      # Expand the current alias
+      aliasName = words[1]
+      cmdLine   = expandAliases(aliasName)
+      
+   return str(cursor) + sep + cmdLine
+   
 # Main program
+
 numArgs = len(sys.argv)
 if numArgs <= 2:
    print "Error: Need to have the existing command line and the cursor postion as inputs"
@@ -45,40 +88,5 @@ cmdLine = sys.argv[1]
 cursor  = int(sys.argv[2])
 sep     = '__sep__'
 
-lineLen = len(cmdLine)
-if cursor < 0:
-   cursor = 0
-elif (cursor > lineLen):
-   cursor = lineLen
-  
-before = cmdLine[0:cursor]
-after  = cmdLine[cursor:lineLen]
+print expandCmdLine(cmdLine, cursor)
 
-words    = re.split("\s+", before)
-numWords = len(words)
-
-if ( numWords == 1 and words[0] == "pl" ) or \
-   ( numWords >= 2 and words[numWords-2] != "a" and words[numWords-1] == "pl" ):
-
-   # Expand the string "pl"
-   words[numWords-1] = "perl -pi -e \"s###g\""
-   cmdLine = " ".join(words) + after
-   cursor  = cursor + 13
-
-elif ( numWords >= 2 and words[numWords-2] == "for" ):
-
-   # Expand "for var" into a full for loop.
-   var = words[numWords-1]
-   words[numWords-2] = "for ((" + var + " = ; " + var + " < ; " + var + "++)); " + \
-                       "do ; done" 
-   words[numWords-1] = ""
-   cmdLine = " ".join(words) + after
-   cursor  = cursor + 5
-   
-elif (numWords == 2 and words[0] == "a"):
-
-   # Expand the current alias
-   aliasName = words[1]
-   cmdLine   = expandAliases(aliasName)
-
-print str(cursor) + sep + cmdLine
