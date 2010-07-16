f13d4eb7adebe7ef30a5c88ab44317de4670da39
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-07-16 05:30:16 +0000

    cleanup

--- scripts/cpp/qt/drawpoly/utils.cpp
+++ scripts/cpp/qt/drawpoly/utils.cpp
@@ -11,12 +11,12 @@ using namespace std;
 void utils::extractWindowDims(// inputs
                               int numArgs, char ** args,
                               // outputs
-                              int & widX, int & widY
+                              int & windowWidX, int & windowWidY
                               ){
 
   // Parse the command line arguments '-geo[metry] 500x600'
 
-  widX = 900; widY = 700; // defaults
+  windowWidX = 900; windowWidY = 700; // defaults
    
   for (int s = 1; s < numArgs; s++){
 
@@ -34,20 +34,89 @@ void utils::extractWindowDims(// inputs
     
     pch = strtok (line, delimiter);
     if (pch == NULL) continue;
-    int widX_tmp = atoi(pch);
+    int windowWidX_tmp = atoi(pch);
 
     pch = strtok (NULL, delimiter);
     if (pch == NULL) continue;
-    int widY_tmp = atoi(pch);
+    int windowWidY_tmp = atoi(pch);
     
-    if (widX_tmp > 0 && widY_tmp > 0){
-      widX = widX_tmp;
-      widY = widY_tmp;
+    if (windowWidX_tmp > 0 && windowWidY_tmp > 0){
+      windowWidX = windowWidX_tmp;
+      windowWidY = windowWidY_tmp;
     }
     
   }
   
 }
 
+void utils::parseCmdOptionsLoadData(//inputs
+                                    int argc, char** argv, char * progName,
+                                    int yFactor,
+                                    // outputs
+                                    int & windowWidX, int & windowWidY,
+                                    std::vector<xg_poly>  & polyVec, 
+                                    std::vector<bool>     & plotPointsOnlyVec
+                                    ){
+
+  polyVec.resize(argc);
+  plotPointsOnlyVec.clear();
+
+  // Skip argv[0] as that's the program name
+  extractWindowDims(argc - 1, argv + 1, windowWidX, windowWidY);
+
+  bool plotPointsOnly = false; // plot the edges or just the vertices
+  
+  int numClips = 0;
+  for (int argIter = 1; argIter < argc; argIter++){
+
+    char * filename = argv[argIter];
+
+    if ( strstr(filename, "-h") || strstr(filename, "--h") ||
+         strstr(filename, "-?") ){
+      printUsage(progName);
+      exit(0);
+    }
+
+    if (strlen(filename) == 0) continue;
+
+    if ( strstr(filename, "-p") ){
+      plotPointsOnly = !plotPointsOnly;
+      continue;
+    }
+    
+    //cout << "Reading " << filename << endl;
+    if ( ! polyVec[numClips].read_poly(filename) ) exit(1);
+
+    // Flip the polygons to compensate for Qt's origin
+    // being in the upper-right corner
+    double * xv = (double*)polyVec[numClips].get_xv();
+    double * yv = (double*)polyVec[numClips].get_yv();
+    int numV    = polyVec[numClips].get_totalNumVerts();
+    for (int s = 0; s < numV; s++){
+      xv[s] = xv[s];
+      yv[s] = yFactor*yv[s];
+    }
+
+    // Flip the annotations as well
+    std::vector<anno> annotations = polyVec[numClips].get_annotations();
+    for (int s = 0; s < (int)annotations.size(); s++){
+      annotations[s].y *= yFactor;
+    }
+    polyVec[numClips].set_annotations(annotations);
+
+    plotPointsOnlyVec.push_back(plotPointsOnly);
+    
+    numClips++;
+    
+  }
+
+  if (numClips == 0){
+    cerr << "No polygons to plot" << endl;
+    exit(1);
+  }
+  polyVec.resize(numClips);
+
+  return;
+}
 
   
