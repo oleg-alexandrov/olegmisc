4de27358e6af7bfb9603387952e61ab5a68639be
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-07-16 07:59:00 +0000

    minor

--- scripts/cpp/polyUtils/cutPoly.cpp
+++ scripts/cpp/polyUtils/cutPoly.cpp
@@ -1,6 +1,7 @@
 #include "cutPoly.h"
 #include <cassert>
 #include <algorithm>
+#include <fstream>
 using namespace std;
 using namespace utils;
 
@@ -42,13 +43,18 @@ void utils::cutPoly(// inputs -- the polygons
     cutToHalfSpace(numV, xv + start, yv + start, xur,
                    hPolyX, hPolyY, hPolyNumP);
     
-  }
 
-  // Temporary
-  cutX        = hPolyX;
-  cutY        = hPolyY;
-  cutNumPolys = hPolyNumP;
-  
+    for (int pIter = 0; pIter < (int)hPolyNumP.size(); pIter++){
+      cutNumPolys.push_back( hPolyNumP[pIter] );
+    }
+    
+    for (int vIter = 0; vIter < (int)hPolyX.size(); vIter++){
+      cutX.push_back( hPolyX[vIter] );
+      cutY.push_back( hPolyY[vIter] );
+    }
+    
+  }
+  return;
 }
 
 void utils::cutToHalfSpace(// inputs 
@@ -60,110 +66,143 @@ void utils::cutToHalfSpace(// inputs
                            std::vector< double> & cutY,
                            std::vector< int>    & cutNumPolys){
 
+  // Intersect the polygon with the half-space
+  // nx*x + ny*y <= dotH.
+  // There are four values for the triplet (nx, ny, H):
+  // (-1,  0, xll) -- left cut
+  // ( 1,  0, xur) -- right cut
+  // ( 0, -1, yll) -- bottom cut
+  // ( 0,  1, yur) -- top cut
+  
+  double nx = 1.0, ny = 0.0;
+  double dotH = (nx + ny)*H; // This formula works only for nx*ny == 0.
+  
   vector<valIndex> ptsOnCutline; ptsOnCutline.clear();
   valIndex C;
   
   cutX.clear(); cutY.clear(); cutNumPolys.clear();
 
-  int numCutPts = 0;
+  int cutPtsIndex = 0;
   
   for (int v = 0; v < numV; v++){
 
-    int vn = (v + 1)%numV;
+    int vnext = (v + 1)%numV;
     
-    double x0 = xv[v],  y0 = yv[v];
-    double xn = xv[vn], yn = yv[vn];
+    double xcurr = xv[v],     ycurr = yv[v];
+    double xnext = xv[vnext], ynext = yv[vnext];
 
+    double dotCurr = nx*xcurr + ny*ycurr;
+    double dotNext = nx*xnext + ny*ynext;
     double cutx, cuty;
     
-    if (x0 < H){
+    if (dotCurr < dotH){
 
-      cutX.push_back(x0);
-      cutY.push_back(y0);
-      numCutPts++; 
-
-      if (xn <= H) continue;
+      // The current point is inside the half-plane
       
-      cutEdge(x0, y0, xn, yn, H, cutx, cuty);
+      cutX.push_back(xcurr);
+      cutY.push_back(ycurr);
+      cutPtsIndex++; 
+      
+      if (dotNext <= dotH) continue;
+      
+      cutEdge(xcurr, ycurr, xnext, ynext, dotH, cutx, cuty);
       cutX.push_back(cutx);
       cutY.push_back(cuty);
 
-      C.val = cuty; C.index = numCutPts;
+      C.val = cuty; C.index = cutPtsIndex;
       ptsOnCutline.push_back(C);
 
-      numCutPts++; 
+      cutPtsIndex++; 
       
-    }else if (x0 > H){
+    }else if (dotCurr > dotH){
 
-      if (xn >= H) continue;
+      // The current point is outside the half-plane
+      
+      if (dotNext >= dotH) continue;
 
-      cutEdge(x0, y0, xn, yn, H, cutx, cuty);
+      cutEdge(xcurr, ycurr, xnext, ynext, dotH, cutx, cuty);
       cutX.push_back(cutx);
       cutY.push_back(cuty);
       
-      C.val = cuty; C.index = numCutPts;
+      C.val = cuty; C.index = cutPtsIndex;
       ptsOnCutline.push_back(C);
 
-      numCutPts++; 
+      cutPtsIndex++; 
 
-    }else if (x0 == H){
+    }else if (dotCurr == dotH){
 
-      int vp = (v == 0) ? (numV - 1) : (v - 1);
-      double xp = xv[vp]; // yp = yv[vp];
+      // The current point is at the edge of the half-plane
+
+      int    vprev   = (v == 0) ? (numV - 1) : (v - 1);
+      double xprev   = xv[vprev], yprev = yv[vprev];
+      double dotPrev = nx*xprev + ny*yprev;
       
-      if (xp >= H && xn >= H) continue;
+      if (dotPrev >= dotH && dotNext >= dotH) continue;
       
-      cutX.push_back(x0);
-      cutY.push_back(y0);
+      cutX.push_back(xcurr);
+      cutY.push_back(ycurr);
 
-      if (xp >= H || xn >= H){
-        C.val = y0; C.index = numCutPts;
+      if (dotPrev >= dotH || dotNext >= dotH){
+        C.val = ycurr; C.index = cutPtsIndex;
         ptsOnCutline.push_back(C);
       }
 
-      numCutPts++; 
+      cutPtsIndex++; 
       
     }
     
   }
 
+  
   // Find the connected components in the cut polygons
+  // To do: Move this to its own function.
+  
   vector<double> X, Y;
   vector<int> P;
   X.clear(); Y.clear(); P.clear();
   
   sort( ptsOnCutline.begin(), ptsOnCutline.end(), lessThan );
-  cout << "----" << endl;
+
+#if 0
+  char * file = "cut.xg";
+  cout << "Writing to " << file << endl;
+  ofstream ch(file);
+  for (int s = 0; s < (int)cutX.size(); s++){
+    ch << cutX[s] << ' ' << cutY[s] << endl;
+    ch << "anno " << cutX[s] << ' ' << cutY[s]  << ' ' << s << endl;
+  }
+  ch.close();
+  
   for (int s = 0; s < (int)ptsOnCutline.size(); s++){
-    cout << "point is "
+    cout << "point on cutline is "
          << ptsOnCutline[s].index << ' ' << ptsOnCutline[s].val << endl; 
   }
+#endif
 
   vector<int> wasVisited;
+  int numCutPts = cutX.size();
   wasVisited.assign(numCutPts, 0);
 
-  int start = 54, ptIter = 0, numPtsOnCutline = ptsOnCutline.size();
+  int ptIter = 0, numPtsOnCutline = ptsOnCutline.size();
+
+  // There must be an even number of points on a cutline.  For each
+  // point at which we cross the cut line to the other side there must
+  // be a corresponding point at which we come back.
   assert(numPtsOnCutline%2 == 0);
   
   while(1){
 
     // Stop when all points are visited
-    if (!wasVisited[start]){
-      ptIter = start;
-    }else{
-
-      bool success = false;
-      for (int v = 0; v < numCutPts; v++){
-        if (!wasVisited[v]){
-          ptIter  = v;
-          success = true;
-          break;
-        }
+    bool success = false;
+    for (int v = 0; v < numCutPts; v++){
+      if (!wasVisited[v]){
+        ptIter  = v;
+        success = true;
+        break;
       }
-
-      if (!success) break; 
-      
     }
+
+    if (!success) break; 
 
     int numPtsInComp = 0;
     
@@ -179,16 +218,19 @@ void utils::cutToHalfSpace(// inputs
       Y.push_back(cutY[ptIter]);
       wasVisited[ptIter] = 1;
       numPtsInComp++;
-      cout << "pt " << ptIter << endl;
       
-      if (cutX[ptIter] != H){
+      if (nx*cutX[ptIter] + ny*cutY[ptIter] != dotH){
         // The point is not at the cutline
         ptIter = (ptIter + 1)%numCutPts;
         continue;
       }
 
-      // The point is at the cutline. Find where exactly it is
-      // in the sorted cutline points.
+      // The point is at the cutline. Find where exactly it is in the
+      // sorted cutline points. If it is not among those sorted
+      // cutline points, it means that the polygon only touches the
+      // cutline at that point rather than crossing over to the other
+      // side.
+      // To do: Use here some faster lookup, such as a map.
       int cutlineIter = 0;
       bool success = false;
       for (cutlineIter = 0; cutlineIter < numPtsOnCutline; cutlineIter++){
@@ -197,7 +239,10 @@ void utils::cutToHalfSpace(// inputs
           break;
         }
       }
-      assert(success);
+      if (!success){
+        ptIter = (ptIter + 1)%numCutPts;
+        continue;
+      }
 
       if (cutlineIter%2 == 0){
         // The point ptIter is at the cutline on the way out.
@@ -217,8 +262,8 @@ void utils::cutToHalfSpace(// inputs
 
   } // End iterating over all points
   
-  cutX = X;
-  cutY = Y;
+  cutX        = X;
+  cutY        = Y;
   cutNumPolys = P;
   
 }
