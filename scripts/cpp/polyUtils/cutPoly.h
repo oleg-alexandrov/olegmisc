eb5d7463b533621db17bd70f99a2b60f61335cac
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-07-21 06:25:20 +0000

    major reorg

--- scripts/cpp/polyUtils/cutPoly.h
+++ scripts/cpp/polyUtils/cutPoly.h
@@ -35,6 +35,45 @@ namespace utils{
     bool   isDuplicate;
   };
 
+  inline int iround(double x){ return (int)round(x); }
+  inline int iceil (double x){ return (int)ceil( x); }
+  inline int ifloor(double x){ return (int)floor(x); }
+  inline int isign (double x){
+    if (x > 0) return  1;
+    if (x < 0) return -1;
+    return 0;
+  }
+
+  
+  class dRect{
+    
+  public:
+    dRect(double xll = 0, double yll = 0, double xur = 0, double yur = 0): 
+      m_xll(xll), m_yll(yll), m_xur(xur), m_yur(yur) {}
+
+    double left()   const { return m_xll; }
+    double top()    const { return m_yll; }
+    double right()  const { return m_xur; }
+    double bottom() const { return m_yur; }
+    double width()  const { return m_xur - m_xll;}
+    double height() const { return m_yur - m_yll;}
+
+  private:
+
+    double m_xll;
+    double m_yll;
+    double m_xur;
+    double m_yur;
+  };
+
+  inline void normalize(dRect & R){
+    double left  = std::min(R.left(), R.right());
+    double right = std::max(R.left(), R.right());
+    double top   = std::min(R.top(), R.bottom());
+    double bot   = std::max(R.top(), R.bottom());
+    R            = dRect(left, top, right, bot);
+  }
+  
   void cutPoly(// inputs -- the polygons
                int numPolys, const int * numVerts,
                const double * xv, const double * yv,
