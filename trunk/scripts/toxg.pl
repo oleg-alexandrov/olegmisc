714f206b4de0c344aee96ee03656a4f1ebcc8bb4
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2010-08-12 16:51:30 +0000

    minor

--- trunk/scripts/toxg.pl
+++ trunk/scripts/toxg.pl
@@ -6,24 +6,33 @@ undef $/;          # read one whole file in one scalar
 
 MAIN:{
   
-  my $ctx = 0;
-  my $cty = 0;
-  my $layer = "";
+  my $ctx      = 0;
+  my $cty      = 0;
+  my $layer    = "";
+  my $color    = "red";
   my $metaFile = "meta.txt";
   if ( -e $metaFile){
+
     open(FILE, "<$metaFile") || die "Cannot open file $metaFile $!";
     my $meta = <FILE>;
     close(FILE);
-    if ($meta =~ /center\s+(.*?)\s+(.*?)\n/){
+
+    if ($meta =~ /color\b.*?(\w+)/i){
+      $color = $1;
+    }
+
+    if ($meta =~ /layer\s*(\d+)\s*:\s*(\d+)/i ){
+      $layer = "$1:$2";
+    }
+
+    if ($meta =~ /center\s+(.*?)\s+(.*?)\n/i){
       $ctx = $1;
       $cty = $2;
     }
-    if ($meta =~ /svg file\s*(.*?)\n/ && scalar(@ARGV) < 2 ){
+    if ($meta =~ /svg file\s*(.*?)\n/i && scalar(@ARGV) < 2 ){
       @ARGV = ($1, @ARGV);
     }
-    if ($meta =~ /layer(\s*;\s*\d+\s*:\s*\d+)/ ){
-      $layer = $1;
-    }
+    
   }
   
   if (scalar(@ARGV) < 2){
@@ -34,7 +43,7 @@ MAIN:{
   my $file_svg = $ARGV[0];
   open(FILE, "<$file_svg"); my $text = <FILE>; close(FILE);
 
-  my $out = "color = red\n";
+  my $out = "color = $color\n";
   foreach my $line (split("\n", $text)){
     next unless ($line =~ /\bd=\"(.*?)\"/s);
     $line = $1;
@@ -47,13 +56,16 @@ MAIN:{
     
     my @verts;
     foreach my $vert (split("\n", $line)){
-      next unless ($vert =~ /^([^\s]*?)\s+([^\s]*?)($|\s)/);
+      next unless ($vert =~ /^\s*([^\s]+)\s+([^\s]+)/);
       my $v1 = $1; my $v2 = $2;
-      if ( $v1 !~ /^[\d\.\-]*$/ || $v2 !~ /^[\d\.\-]*$/ ){
+      if ( $v1 !~ /^[e\d\.\-\+]*$/ || $v2 !~ /^[e\d\.\-\+]*$/ ){
         print "Error!\n";
         print "$line\n";
       }
-      $vert = ( floor($v1 + 0.5) + $ctx ) . " " . ( -floor($v2 + 0.5) + $cty ) . $layer;
+      $vert = ( floor($v1 + 0.5) + $ctx ) . " " . ( -floor($v2 + 0.5) + $cty );
+      if ($layer !~ /^\s*$/){
+        $vert .= "; $layer"
+      }
       push(@verts, $vert);
     }
     $line = join("\n", @verts);
@@ -63,7 +75,7 @@ MAIN:{
   }
 
   my $file_xg = $ARGV[1];
-  open(FILE, ">$file_xg"); print FILE $out; close(FILE);
+  open(FILE, ">$file_xg"); print FILE "$out\n"; close(FILE);
 
   print "Reading $file_svg\n";
   print "Writing $file_xg\n";
