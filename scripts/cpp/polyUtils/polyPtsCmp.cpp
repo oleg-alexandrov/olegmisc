55080efc9e4f209cfd05c57fceb3950b9b6e0733
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2011-01-28 23:33:29 +0000

    minor

--- scripts/cpp/polyUtils/polyPtsCmp.cpp
+++ scripts/cpp/polyUtils/polyPtsCmp.cpp
@@ -9,8 +9,8 @@
 
 // Compare two polygons point-by-point. We assume there can be points
 // on edges of polygons. If one polygon has a point repeated twice,
-// but the second one has it repeated just once, that will be flagged
-// as a difference.
+// but the second polygon has it repeated just once, that will be
+// flagged as a difference.
 
 using namespace std;
 
