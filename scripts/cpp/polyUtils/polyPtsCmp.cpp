9e17fb45bd2829fbdb78a02e0467b1df63437463
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2011-01-28 22:44:16 +0000

    minor

--- scripts/cpp/polyUtils/polyPtsCmp.cpp
+++ scripts/cpp/polyUtils/polyPtsCmp.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <vector>
 #include <cassert>
+#include <set>
 #include "dPoly.h"
 
 // Compare two polygons point-by-point. We assume there can be points on
@@ -20,6 +21,24 @@ bool operator< (dPoint P, dPoint Q){
   return ( P.x < Q.x ) || (P.x == Q.x && P.y < Q.y);
 }
 
+void putPolyInMultiSet(const dPoly & P, multiset<dPoint> & mP){
+
+  const double * x = P.get_xv();
+  const double * y = P.get_yv();
+
+  mP.clear();
+  
+  int numVerts = P.get_totalNumVerts();
+  for (int v = 0; v < numVerts; v++){
+    dPoint P;
+    P.x = x[v];
+    P.y = y[v];
+    mP.insert(P);
+  }
+
+  return;
+}
+
 int main (int argc, char ** argv){
 
   if (argc < 3){
@@ -27,9 +46,36 @@ int main (int argc, char ** argv){
     exit(1);
   }
 
-  dPoly P; P.readPoly(argv[1]);
-  dPoly Q; Q.readPoly(argv[2]);
+  dPoly P; P.readPoly(argv[1]); multiset<dPoint> mP; putPolyInMultiSet(P, mP);
+  dPoly Q; Q.readPoly(argv[2]); multiset<dPoint> mQ; putPolyInMultiSet(Q, mQ);
+
+  // Put here points in both multi-sets
+  vector<dPoint> shared;
+  shared.clear();
+
+  multiset<dPoint>::iterator it;
+  for (it = mP.begin(); it != mP.end(); it++){
+    if ( mQ.find(*it) != mQ.end() ) shared.push_back(*it);
+  }
+
+  for (int s = 0; s < (int)shared.size(); s++) mP.erase(shared[s]);
+  for (int s = 0; s < (int)shared.size(); s++) mQ.erase(shared[s]);
+
+  char *outFile = "diff.xg", *color1 = "red", *color2 = "green";
+
+  cout << "Writing the differences to " << outFile << endl;
+  cout << "Points in " << argv[1] << ' ' << " not in " << argv[2] << " are in "
+       << color1  << endl;
+  cout << "Points in " << argv[2] << ' ' << " not in " << argv[1] << " are in "
+       << color2  << endl;
   
+  ofstream of(outFile);
+  of << "color = " << color1 << endl;
+  for (it = mP.begin(); it != mP.end(); it++) of << it->x << ' ' << it->y << endl;
+  of << "color = " << color2 << endl;
+  for (it = mQ.begin(); it != mQ.end(); it++) of << it->x << ' ' << it->y << endl;
+  of.close();
+
   return 0;
 
 }
