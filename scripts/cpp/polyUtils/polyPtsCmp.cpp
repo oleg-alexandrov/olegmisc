0bdfeaa29fe54477b9f935082d21b018eea20474
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2011-03-08 03:26:45 +0000

    minor

--- scripts/cpp/polyUtils/polyPtsCmp.cpp
+++ scripts/cpp/polyUtils/polyPtsCmp.cpp
@@ -4,9 +4,9 @@
 #include <algorithm>
 #include <vector>
 #include <cassert>
-#include <set>
 #include "dPoly.h"
 #include "dPoint.h"
+#include "geomUtils.h"
 
 // Compare two polygons point-by-point. We assume there can be points
 // on edges of polygons. If one polygon has a point repeated twice,
@@ -14,24 +14,7 @@
 // flagged as a difference.
 
 using namespace std;
-
-void putPolyInMultiSet(const dPoly & P, multiset<dPoint> & mP){
-
-  const double * x = P.get_xv();
-  const double * y = P.get_yv();
-
-  mP.clear();
-  
-  int numVerts = P.get_totalNumVerts();
-  for (int v = 0; v < numVerts; v++){
-    dPoint P;
-    P.x = x[v];
-    P.y = y[v];
-    mP.insert(P);
-  }
-
-  return;
-}
+using namespace utils;
 
 int main (int argc, char ** argv){
 
@@ -40,38 +23,25 @@ int main (int argc, char ** argv){
     exit(1);
   }
 
+  vector<dPoint>  vP, vQ;
+  
   char *inFile1 = argv[1], *inFile2 = argv[2];
-  dPoly P; P.readPoly(inFile1); multiset<dPoint> mP; putPolyInMultiSet(P, mP);
-  dPoly Q; Q.readPoly(inFile2); multiset<dPoint> mQ; putPolyInMultiSet(Q, mQ);
+  dPoly P; P.readPoly(inFile1);
+  dPoly Q; Q.readPoly(inFile2);
 
-  // If a point is in mP, and also in mQ, mark it as being in mP and wipe it from mQ
-  vector<dPoint> shared;
-  shared.clear();
-  multiset<dPoint>::iterator ip, iq;
-  for (ip = mP.begin(); ip != mP.end(); ip++){
-    iq = mQ.find(*ip);
-    if ( iq != mQ.end() ){
-      shared.push_back(*ip);
-      mQ.erase(iq); // Erase just the current instance of the given value
-    }
-  }
-  
-  // Wipe it from mP as well
-  for (int s = 0; s < (int)shared.size(); s++){
-    ip = mP.find(shared[s]);
-    if ( ip != mP.end() ){
-      mP.erase(ip); // Erase just the current instance of the given value
-    }
-  }
-  
+  findPolyDiff(P, Q,  // inputs
+               vP, vQ // outputs
+               );
+
   string outFile1 = "diff1.xg", outFile2 = "diff2.xg", color1 = "red", color2 = "green";
   cout << "Writing points in " << inFile1 << ' ' << "not in " << inFile2 << " to "
        << outFile1 << endl;
   ofstream of1(outFile1.c_str());
   of1 << "color = " << color1 << endl;
   int count1 = 0;
-  for (ip = mP.begin(); ip != mP.end(); ip++){
-    of1 << ip->x << ' ' << ip->y << endl;
+  for (int s = 0; s < (int)vP.size(); s++){
+    dPoint p = vP[s];
+    of1 << p.x << ' ' << p.y << endl;
     count1++;
   }
   of1.close();
@@ -82,8 +52,9 @@ int main (int argc, char ** argv){
   int count2 = 0;
   ofstream of2(outFile2.c_str());
   of2 << "color = " << color2 << endl;
-  for (iq = mQ.begin(); iq != mQ.end(); iq++){
-    of2 << iq->x << ' ' << iq->y << endl;
+  for (int s = 0; s < (int)vQ.size(); s++){
+    dPoint q = vQ[s];
+    of2 << q.x << ' ' << q.y << endl;
     count2++;
   }
   of2.close();
