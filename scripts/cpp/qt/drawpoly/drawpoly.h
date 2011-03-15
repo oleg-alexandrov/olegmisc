8ca238b4d1eb94f2c9437985e6bff54d2d2c1770
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2011-03-15 06:09:26 +0000

    minor

--- scripts/cpp/qt/drawpoly/drawpoly.h
+++ scripts/cpp/qt/drawpoly/drawpoly.h
@@ -32,8 +32,7 @@ public slots:
   void saveMultiplePoly(bool overwrite);
   void shiftPolys();
   void rotatePolys();
-  void shiftPolys(std::vector<double> & shifts);
-  void rotatePolys(std::vector<double> & angle);
+  void scalePolys();
 
   // View menu
   void zoomOut();
@@ -83,6 +82,9 @@ private:
   void printCmd(std::string cmd, double xll, double yll,
                 double widX, double widY);
   void printCmd(std::string cmd);
+  void shiftPolys(std::vector<double>  & shifts);
+  void rotatePolys(std::vector<double> & angle);
+  void scalePolys(std::vector<double>  & scale);
   void drawMark(int x0, int y0, QColor color, int lineWidth,
                 QPainter * paint);
   void setupDisplayOrder(int                 numPolys, 
