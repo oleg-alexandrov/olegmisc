fee9ef682c39a9d8019197ddef75aece3f88a4f5
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2011-03-25 05:20:34 +0000

    minor

--- trunk/scripts/cpp/polyView/geom/test_kdTree.cpp
+++ trunk/scripts/cpp/polyView/geom/test_kdTree.cpp
@@ -12,23 +12,27 @@ using namespace utils;
 
 int main(int argc, char** argv){
 
-  dPoly poly;
-  if (argc <= 1){
-    cout << "Usage: " << argv[0] << " file.xg" << endl;
+  if (argc < 3){
+    cout << "Usage: " << argv[0] << " poly.xg box.xg" << endl;
     exit(1);
   }
   
-  char * filename = argv[1];
-  cout << "Reading " << filename << endl;
+  char * polyFile = argv[1];
+  char * boxFile  = argv[2];
+  cout << "Reading " << polyFile << " and " << boxFile << endl;
   
   bool isPointCloud = false;
 
-  if (! poly.readPoly(filename, isPointCloud) ) exit(1);
+  dPoly poly, box;
+  if (! poly.readPoly(polyFile, isPointCloud) ) exit(1);
+  if (! box.readPoly(boxFile,   isPointCloud) ) exit(1);
 
   double * xv   = (double*)poly.get_xv(); // to do: fix this hack
   double * yv   = (double*)poly.get_yv();
   int      numV = poly.get_totalNumVerts();
 
+  double xl, yl, xh, yh; 
+  box.bdBox(xl, yl, xh, yh);
 
   vector<Point> Pts;
   Pts.reserve(numV);
@@ -43,7 +47,6 @@ int main(int argc, char** argv){
   T.formTree(Pts); 
 
   vector<Point> outPts; // Must be different than Pts
-  double xl = 0, yl = 1, xh = 8, yh = 7;
   T.getPointsInBox(xl, yl, xh, yh, outPts);
 
   vector<Point> outPts2;
@@ -58,6 +61,14 @@ int main(int argc, char** argv){
   for (int s = 0; s < (int)outPts.size(); s++)
     cout << outPts[s].x << ' ' << outPts[s].y << endl;
 
+  string outFile = "ptsInBox.xg";
+  cout << "Writing to " << outFile << endl;
+  ofstream of(outFile.c_str());
+  of << "color = white" << endl;
+  for (int s = 0; s < (int)outPts.size(); s++)
+    of << outPts[s].x << ' ' << outPts[s].y << endl;
+  of.close();
+  
   cout << endl << "Points in outPts2" << endl;
   for (int s = 0; s < (int)outPts2.size(); s++)
     cout << outPts2[s].x << ' ' << outPts2[s].y << endl;
