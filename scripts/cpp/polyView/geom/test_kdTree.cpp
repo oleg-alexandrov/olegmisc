267eeaf6f0b5af7bcf7f7b12a31a0173f0efd266
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2011-03-25 05:13:08 +0000

    minor

--- scripts/cpp/polyView/geom/test_kdTree.cpp
+++ scripts/cpp/polyView/geom/test_kdTree.cpp
@@ -38,9 +38,29 @@ int main(int argc, char** argv){
   }
   
   kdTree T;
-  T.formTree(Pts); // Pts will be reordered but otherwise unchanged inside of this function
+  // Pts will be reordered but otherwise unchanged inside of this function.
+  // Do not modify this vector afterward.
+  T.formTree(Pts); 
+
+  vector<Point> outPts; // Must be different than Pts
+  double xl = 0, yl = 1, xh = 8, yh = 7;
+  T.getPointsInBox(xl, yl, xh, yh, outPts);
+
+  vector<Point> outPts2;
+  outPts2.clear();
+  for (int s = 0; s < numV; s++){
+    const Point & P = Pts[s]; // alias
+    if (xl <= P.x && P.x <= xh && yl <= P.y && P.y <= yh) outPts2.push_back(P);
+  }
+
+
+  cout << "Points in outPts" << endl;
+  for (int s = 0; s < (int)outPts.size(); s++)
+    cout << outPts[s].x << ' ' << outPts[s].y << endl;
+
+  cout << endl << "Points in outPts2" << endl;
+  for (int s = 0; s < (int)outPts2.size(); s++)
+    cout << outPts2[s].x << ' ' << outPts2[s].y << endl;
   
-    
   return 0;
-  
 }
