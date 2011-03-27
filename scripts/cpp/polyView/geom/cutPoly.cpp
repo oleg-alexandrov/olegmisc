37d201619a0390ed4f760fad4b2d2bf54cd14f35
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2011-03-27 02:41:48 +0000

    minor

--- scripts/cpp/polyView/geom/cutPoly.cpp
+++ scripts/cpp/polyView/geom/cutPoly.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <fstream>
 #include <cfloat>
+#include "edgeUtils.h"
+
 using namespace std;
 using namespace utils;
 
@@ -311,48 +313,6 @@ void utils::cutToHalfSpace(// inputs
   after.close();
 #endif
   
-}
-
-void utils::cutEdge(double x0, double y0, double x1, double y1,
-                    double nx, double ny, double H,
-                    double & cutx, double & cuty){
-
-  // Find the point at which the line nx*x + ny*y = H intersects the
-  // edge (x0, y0) --> (x1, y1).
-
-  double dot0 = nx*x0 + ny*y0;
-  double dot1 = nx*x1 + ny*y1;
-  
-  assert( (dot0 <= H && dot1 >= H) || (dot0 >= H && dot1 <= H ) );
-  assert( dot0 != dot1 );
-
-  // Find t such that (1-t)*(x0, y0) + t*(x1, y1) intersect the
-  // cutting line
-  
-  double t = (H - dot0)/(dot1 - dot0);
-  t = max(t, 0.0); t = min(t, 1.0); // extra precautions
-  cutx = (1-t)*x0 + t*x1;
-  cuty = (1-t)*y0 + t*y1;
-
-  // Cut symmetrically in x0 and x1 to avoid problems later
-  t = (H - dot1)/(dot0 - dot1);
-  t = max(t, 0.0); t = min(t, 1.0); // extra precautions
-  double cutx2 = (1-t)*x1 + t*x0;
-  double cuty2 = (1-t)*y1 + t*y0;
-
-  cutx = 0.5*cutx + 0.5*cutx2;
-  cuty = 0.5*cuty + 0.5*cuty2;
-  
-  // The above formulas have floating point errors. Use the fact that
-  // the cutting line is either vertical or horizontal to recover
-  // precisely one of the two coordinates above.
-  if (nx == 0){
-    cuty = H/ny;
-  }else if (ny == 0){
-    cutx = H/nx;
-  }
-  
-  return;
 }
 
 void utils::processPointsOnCutline(std::vector<valIndex> & ptsOnCutline){
---
+++ scripts/cpp/polyView/geom/edgeUtils.h
@@ -0,0 +1,12 @@
+#ifndef EDGE_UTILS_H
+#define EDGE_UTILS_H
+
+namespace utils {
+  void cutEdge(double x0, double y0, double x1, double y1,
+               double nx, double ny, double H,
+               double & cutx, double & cuty);
+  
+}
+
+#endif
+
