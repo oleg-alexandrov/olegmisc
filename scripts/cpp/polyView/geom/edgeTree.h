270acc94b278817b873b74f46ba1aa1472177568
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2011-04-12 17:33:52 +0000

    minor

--- scripts/cpp/polyView/geom/edgeTree.h
+++ scripts/cpp/polyView/geom/edgeTree.h
@@ -15,6 +15,7 @@ public:
   void findPolyEdgesInBox(// inputs
                           double xl, double yl,
                           double xh, double yh,
+                          // outputs
                           std::vector<utils::seg> & edgesInBox
                           );
   
@@ -27,24 +28,26 @@ public:
   
 private:
 
-  inline void edgeToBdBox(//inputs
-                          double bx, double by, double ex, double ey,
-                          // output
-                          utils::dRectWithId & R){
+  inline void edgeToBox(//inputs
+                        double bx, double by, double ex, double ey,
+                        // outputs
+                        utils::dRectWithId & R
+                        ){
     // Given an edge, compute its bounding box and the info necessary info
-    // to reverse this transformation.
+    // to reverse this transformation, see boxToEdge.
     int id = 0;
     if (bx > ex){ std::swap(bx, ex); id |= 1; } // id = id | 01
     if (by > ey){ std::swap(by, ey); id |= 2; } // id = id | 10
     R = utils::dRectWithId(bx, by, ex, ey, id);
     return;
   }
-
-  inline void bdBoxToEdge(// input
-                          const utils::dRectWithId & R, 
-                          // outputs
-                          double & bx, double & by, double & ex, double & ey){
-    // Recover the edge based on the bounding box and id.
+  
+  inline void boxToEdge(// inputs
+                        const utils::dRectWithId & R, 
+                        // outputs
+                        double & bx, double & by, double & ex, double & ey
+                        ){
+    // Recover the edge based on the bounding box and id. The inverse of edgeToBox.
     bx = R.xl; by = R.yl; ex = R.xh; ey = R.yh;
     int id = R.id;
     if (id & 1) std::swap (bx, ex);
