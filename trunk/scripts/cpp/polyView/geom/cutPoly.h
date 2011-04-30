3555c12940bbbc3b058ab03d878ba1ca2dae1cfd
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2011-04-30 23:26:50 +0000

    minor

--- trunk/scripts/cpp/polyView/geom/cutPoly.h
+++ trunk/scripts/cpp/polyView/geom/cutPoly.h
@@ -22,8 +22,8 @@ namespace utils{
                int numPolys, const int * numVerts,
                const double * xv, const double * yv,
                // inputs -- the cutting window
-               double xll, double yll,
-               double xur, double yur,
+               double xll, double yll, double xur, double yur,
+               bool isPolyClosed,
                // outputs -- the cut polygons
                std::vector< double> & cutX,
                std::vector< double> & cutY,
@@ -37,6 +37,7 @@ namespace utils{
                       double nx, double ny, double dotH,
                       int numV, 
                       const double * xv, const double * yv,
+                      bool isPolyClosed,
                       // outputs -- the cut polygons
                       std::vector<double> & cutX,
                       std::vector<double> & cutY,
