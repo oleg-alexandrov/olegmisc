905f5701a50a66b0f3dba3470efcb8e82ffe0c82
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2011-05-18 05:55:19 +0000

    minor

--- scripts/cpp/polyView/gui4/actions.sh
+++ scripts/cpp/polyView/gui4/actions.sh
@@ -7,3 +7,4 @@ perl -pi -e "s#([ ]*\/\/\s*actions.*?\n)#  void $fun       \(\);\n\$1#g" appWind
 
 perl -pi -e "s#(\/\/\s*actions.*?\n)#void polyView::$fun\(\)\{\n  \n\n\}\n\n\$1#g" polyView.cpp
 perl -pi -e "s#(\/\/\s*actions.*?\n)#void appWindow::$fun       \(\)\{ m_poly\-\>$1       \(\); \}\n\$1#g" appWindow.cpp
+test
