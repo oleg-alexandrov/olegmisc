63cb50ad431aa46eca5162e067a9cebd95781ad1
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2011-06-22 06:58:42 +0000

    minor

---
+++ trunk/scripts/cpp/quine.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std; int main(){
+vector<string> L; L.resize(300); int i = 0; char n = 10, q = 34, s = 59;
+L[i++] = n; L[i++] = q; L[i++] = s;
+L[i++] = "L[i++] = ";
+L[i++] = "#include <iostream>";
+L[i++] = "#include <string>";
+L[i++] = "#include <vector>";
+L[i++] = "using namespace std; int main(){";
+L[i++] = "vector<string> L; L.resize(300); int i = 0; char n = 10, q = 34, s = 59;";
+L[i++] = "L[i++] = n; L[i++] = q; L[i++] = s;";
+L[i++] = "for (int s = 0; s < int(sizeof(a)/sizeof(int)); s++) cout << L[a[s]]; return 0;}";
+L[i++] = "int a[] = {4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 3, 1, 3, 1, 2, 0,  3, 1, 4, 1, 2, 0, 3, 1, 5, 1, 2, 0, 3, 1, 6, 1, 2, 0, 3, 1, 7, 1, 2, 0, 3, 1, 8, 1, 2, 0, 3, 1, 9, 1, 2, 0, 3, 1, 10, 1, 2, 0, 3, 1, 11, 1, 2, 0, 11, 0,10, 0,};";
+int a[] = {4, 0, 5, 0, 6, 0, 7, 0, 8, 0, 9, 0, 3, 1, 3, 1, 2, 0,  3, 1, 4, 1, 2, 0, 3, 1, 5, 1, 2, 0, 3, 1, 6, 1, 2, 0, 3, 1, 7, 1, 2, 0, 3, 1, 8, 1, 2, 0, 3, 1, 9, 1, 2, 0, 3, 1, 10, 1, 2, 0, 3, 1, 11, 1, 2, 0, 11, 0,10, 0,};
+for (int s = 0; s < int(sizeof(a)/sizeof(int)); s++) cout << L[a[s]]; return 0;}
