6bf3f9e76cc6a3684e132ece76dfb63c156adf0d
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2011-10-13 05:45:34 +0000

    minor

--- scripts/cpp/lambda_test.cpp
+++ scripts/cpp/lambda_test.cpp
@@ -2,8 +2,14 @@
 #include <vector>
 #include "boost/lambda/lambda.hpp"
 #include "boost/function.hpp"
+#include "boost/bind.hpp"
 using namespace std;
-using namespace boost::lambda;
+using namespace boost;
+//using namespace boost;
+
+double f2(int x, int y){
+  return x + y;
+}
 
 int main() {
 
@@ -12,14 +18,49 @@ int main() {
   a.push_back(2);
   a.push_back(4);
 
-  for_each( a.begin(), a.end(), std::cout << _1 << "\n" );
+  cout << "original values" << endl;
+  for_each( a.begin(), a.end(), std::cout << lambda::_1 << "\n" );
+  for_each( a.begin(), a.end(), lambda::_1 += 10 );
+  cout << "added values" << endl;
+  for_each( a.begin(), a.end(), std::cout << lambda::_1 << "\n" );
 
-  //(std::cout << _1 << "\n")("xuxa");
-  //(std::cout << _1 << std::endl)("xuxa");
-//   (std::cout << _1 << " " << _3 << " " << _2 << "!\n")
+  cout << "value is " << (lambda::_1 * lambda::_1 + lambda::_2)(3, 4) << endl;
+  
+  boost::function<int(int,int)> f = lambda::_1 * lambda::_1 + lambda::_2;
+  cout << "value 2 is " << f(3, 4) << endl;
+  
+  boost::function<int(int,int)> g = lambda::_1 - lambda::_2;
+
+  vector< boost::function<int(int,int)> > V;
+  V.push_back(f);
+  V.push_back(g);
+
+  for (int s = 0; s < (int)V.size(); s++){
+    cout << "value is " << V[s](3, 4) << endl;
+  }
+
+  cout << "value before is " << f(3, 4) << endl;
+  cout << "pointers before: " << &f << ' ' << &g << endl;
+  f = g;
+  cout << "value after is " << f(3, 4) << endl;
+  cout << "pointers after: " << &f << ' ' << &g << endl;
+  
+  //(std::cout << lambda::_1 << "\n")("xuxa");
+  //(std::cout << lambda::_1 << std::endl)("xuxa");
+//   (std::cout << lambda::_1 << " " << _3 << " " << lambda::_2 << "!\n")
 //     ("Hello","friend","my");
 
 
-    
+  // Mixing bind and lambda
+  boost::function<int(int,int)> h = lambda::_1 + lambda::_2;
+
+  cout << "value 1 is " << bind(f2, _1, 7)(2) << endl; // Same as f(2, 7)
+  cout << "value 2 is " << bind(h,  _1, 7)(2) << endl; // Same as f(2, 7)
+  
+  boost::function<int(int)>  k = bind(h, _1, 7);
+  cout << "value 3 is " << k(2) << endl;
+
+  //boost::function<int(int,int)>  k2 = function<int(int,int)>(h, lambda::_1, lambda::_2);
+  
   return 0;
 }
