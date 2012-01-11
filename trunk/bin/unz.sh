e70f1eefdf8220b5eac27bd7fc0e7489e9f78a02
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2012-01-11 19:04:01 +0000

    minor

--- trunk/bin/unz.sh
+++ trunk/bin/unz.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
- 
+
 case "$1" in
     *.tar.z|*.tar.gz|*.tgz)  tar xzfv  $1;;
     *.tar)                   tar xfv   $1;;
-    *.Z|*.z|*.gz)                gunzip    $1;;
+    *.Z|*.z|*.gz)            gunzip    $1;;
     *.tar.bz2)               bzip2 -dc $1 | tar xfv - ;;
     *.bz2)                   bzip2 -d  $1;;
     *.zip)                   unzip     $1;;
