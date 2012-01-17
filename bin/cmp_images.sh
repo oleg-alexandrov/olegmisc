e842a0e9c30442994bb34679f7b76387177c1649
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2012-01-17 19:30:07 +0000

    minor

--- bin/cmp_images.sh
+++ bin/cmp_images.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+plainDiff=0
+if [ "$#" -eq 3 ]; then 
+    shift
+    plainDiff=1
+fi
+
 echo $1 > /tmp/img1.txt
 echo $2 > /tmp/img2.txt
 gdalinfo -stats $1 >> /tmp/img1.txt
 gdalinfo -stats $2 >> /tmp/img2.txt
 
-tkdiff /tmp/img1.txt /tmp/img2.txt
+if [ "$plainDiff" -eq 1 ]; then
+    diff /tmp/img1.txt /tmp/img2.txt
+else
+    tkdiff /tmp/img1.txt /tmp/img2.txt
+fi
 
 rm -f /tmp/img1.txt /tmp/img2.txt
