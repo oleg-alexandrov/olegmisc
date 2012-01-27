356e4eba20fc14e5e965f882b2c6b957e18ef520
oleg.alexandrov <oleg.alexandrov@75bb846d-1250-0410-b7c0-c13add02b071>
2012-01-27 16:48:36 +0000

    minor

--- bin/rsync.sh
+++ bin/rsync.sh
@@ -7,6 +7,7 @@ source ~/.bashenv
 # Copy only files matching the given patterns
 rsync -avz                                     \
     --exclude "Downloads/*"                    \
+    --exclude ".svn/*"                         \
     --exclude "projects/base_system*"          \
     --include-from=$HOME/bin/rsync_include.txt \
     --exclude "*"                              \
@@ -18,6 +19,6 @@ rsync -avz $B:gitserver $HOME
 
 for dir in visionworkbench StereoPipeline; do 
     rsync -avz --exclude "*.o" --exclude ".deps" \
-        --exclude ".libs*" --exclude "tests/*"   \
+        --exclude ".libs*"                       \
         $B:projects/$dir $HOME/projects
 done
