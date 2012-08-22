e4178be51ebecbc24b098d2d63ea7bf1080029ca
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2012-08-22 05:22:45 +0000

    minor

--- bin/print_col.pl
+++ bin/print_col.pl
@@ -12,13 +12,20 @@ MAIN:{
     exit(0);
   }
 
+  my @cols;
+  foreach my $num (@ARGV){
+    $num -= 1; # start from 0 instead of 1
+    push(@cols, $num);
+  }
+  
   foreach my $line (<STDIN>){
     $line =~ s/^\s*//g;
     $line =~ s/\s*$//g;
     my @vals = split(/\s+/, $line);
-    foreach my $num (@ARGV){
-      next unless ($num =~ /^\d+$/ && scalar(@vals) >= $num);
-      print $vals[$num - 1] . "\t";
+    foreach my $num (@cols){
+      $num += scalar(@vals) if ($num < 0); # treat -1 as last element
+      next unless ($num =~ /^\d+$/ && $num < scalar(@vals) );
+      print $vals[$num] . "\t";
     }
     print "\n";
   }
