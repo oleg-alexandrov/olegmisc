8a8dfaf9de238dd84293ed5123907d45b4a338f4
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2012-08-22 05:58:15 +0000

    minor

--- trunk/python/expand_cmdline.py
+++ trunk/python/expand_cmdline.py
@@ -59,15 +59,30 @@ def expandCmdLine(cmdLine, cursor):
    numWords = len(words)
 
    for k in range(numWords):
-
       if ( words[k] == "foo" ):
          # Replace foo with someOtherFooBar
          words[k] = "someOtherFooBar"
       cmdLine = " ".join(words) + after
       cursor  = len(cmdLine)
-       
-   if    ( numWords >= 1 and words[numWords-1] == "21"):
+
+   lWord = words[numWords-1]    
+   if    ( numWords >= 2 and words[numWords-1] == "p"):
+      words[numWords-1] = words[numWords-2] # replace with the preceding word
+      cmdLine = " ".join(words) + after
+      cursor  = len(cmdLine)
+      
+   elif  ( numWords >= 1 and words[numWords-1] == "21"):
       words[numWords-1] = "2>&1&" # expand '21' into '2>&1&'
+      cmdLine = " ".join(words) + after
+      cursor  = len(cmdLine)
+      
+   elif  ( numWords >= 1 and words[numWords-1] == "o2"):
+      words[numWords-1] = "> output.txt 2>&1&"
+      cmdLine = " ".join(words) + after
+      cursor  = len(cmdLine)
+
+   elif  ( numWords >= 1 and words[numWords-1] == "ss"):
+      words[numWords-1] = "*/*h */*cc" # expand 'ss' into '*/*h */*cc'
       cmdLine = " ".join(words) + after
       cursor  = len(cmdLine)
       
@@ -77,6 +92,7 @@ def expandCmdLine(cmdLine, cursor):
       cursor  = len(cmdLine)
       
    elif  ( numWords >= 3 and words[numWords-3] == "r"):
+      #print('now here')
       # Given the input 'hi there r hi ho', will replace it with 'ho there'  
       fr = words[numWords-2]
       to = words[numWords-1]
@@ -92,7 +108,6 @@ def expandCmdLine(cmdLine, cursor):
       
    elif ( numWords == 1 and words[0] == "pl" ) or \
           ( numWords >= 2 and words[numWords-2] != "a" and words[numWords-1] == "pl" ):
-
       # Expand the string "pl"
       words[numWords-1] = "perl -pi -e \"s###g\""
       cmdLine = " ".join(words) + after
@@ -108,11 +123,20 @@ def expandCmdLine(cmdLine, cursor):
       cursor  = cursor + 5
       
    elif (numWords == 2 and words[0] == "a"):
-
       # Expand the current alias
       aliasName = words[1]
       cmdLine   = expandAliases(aliasName)
-      
+
+   elif ( numWords >= 1 and lWord >= "1" and lWord <= "9" and len(lWord) == 1):
+      # Read the file, paste the last item on specified line from the end.
+      # This is useful to do paste without using the mouse.
+      f = open('/tmp/output_llt.txt', 'r')
+      a=f.readlines()
+      pos = int(words[numWords-1])
+      words[numWords-1] = a[-pos].split(" ")[-1]
+      cmdLine = " ".join(words) + after
+      cursor  = len(cmdLine)
+   
    return str(cursor) + sep + cmdLine
    
 # Main program
