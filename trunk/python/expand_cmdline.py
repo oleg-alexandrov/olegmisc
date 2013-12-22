95ecb8cb112cb19c1698f2d3ea42204bc5d7528a
oleg.alexandrov@gmail.com <oleg.alexandrov@gmail.com@75bb846d-1250-0410-b7c0-c13add02b071>
2013-12-22 05:59:44 +0000

    minor

--- trunk/python/expand_cmdline.py
+++ trunk/python/expand_cmdline.py
@@ -11,28 +11,28 @@ def expandAlias(aliasName):
 
    # Given the name of an alias, return the line showing its definition
    # in the alias file
-   
+
    HOME     = os.getenv("HOME")
    fileName = HOME + '/.bash_aliases'
 
    FILE = open(fileName, "r")
    data = FILE.read()
    FILE.close()
-   
+
    allAliases     = data.split("\n")
    aliasExpansion = "a " + aliasName
-   
+
    for line in allAliases:
 
        matches = re.match('^(alias ' + aliasName + '=.*?)$', line)
        if matches:
            aliasExpansion = matches.group(1)
        # end if
-    
+
    #end for
-   
+
    aliasExpansion = re.sub('^alias', 'a', aliasExpansion)
-   
+
    return aliasExpansion
 
 # end expandAlias()
@@ -41,20 +41,20 @@ def expandCmdLine(cmdLine, cursor):
 
    # Intelligently replace the current string with another string.
    # We take into account where the cursor is positioned in the string.
-   
+
    lineLen = len(cmdLine)
    if cursor < 0:
       cursor = 0
    elif (cursor > lineLen):
       cursor = lineLen
-  
+
    before = cmdLine[0:cursor]
    after  = cmdLine[cursor:lineLen]
 
    #marker = ' xtk45TSfl ';
    #words = re.sub(r'\b', marker, before).split(marker);
    words    = re.split(" ", before)
-   
+
    numWords = len(words)
 
    for k in range(numWords):
@@ -64,7 +64,12 @@ def expandCmdLine(cmdLine, cursor):
       cmdLine = " ".join(words) + after
       cursor  = len(cmdLine)
 
-   lWord = words[numWords-1]    
+   lWord = words[numWords-1]
+   if    ( numWords >= 2 and lWord[-2:] == "ld" and (len(lWord) == 2 or not re.match('\w', lWord[-3]))):
+      words[numWords-1] = lWord[:-2] + "LD_LIBRARY_PATH"
+      cmdLine = " ".join(words) + after
+      cursor  = len(cmdLine)
+
    if    ( numWords >= 2 and words[0] == "hp"):
       # If the first word is hp, expand the abbrevation
       # of the second word
@@ -74,17 +79,17 @@ def expandCmdLine(cmdLine, cursor):
          words[1] = matches.group(1)
          cmdLine = " ".join(words) + after
          cursor  = len(cmdLine)
-      
+
    if  ( numWords >= 2 and words[numWords-1] == "p"):
       words[numWords-1] = words[numWords-2] # replace with the preceding word
       cmdLine = " ".join(words) + after
       cursor  = len(cmdLine)
-      
+
    elif  ( numWords >= 1 and words[numWords-1] == "21"):
       words[numWords-1] = "2>&1&" # expand '21' into '2>&1&'
       cmdLine = " ".join(words) + after
       cursor  = len(cmdLine)
-      
+
    elif  ( numWords >= 1 and words[numWords-1][:1] == "o"):
       # Replace o45 with > output45.txt 2>&1&
       suff = words[numWords-1][1:]
@@ -96,15 +101,15 @@ def expandCmdLine(cmdLine, cursor):
       words[numWords-1] = "*/*h */*cc" # expand 'ss' into '*/*h */*cc'
       cmdLine = " ".join(words) + after
       cursor  = len(cmdLine)
-      
+
    elif  ( numWords >= 1 and words[numWords-1] == "rs"):
       words[numWords-1] = "rsync -avz" # expand 'rs' into 'rsync -avz'
       cmdLine = " ".join(words) + after
       cursor  = len(cmdLine)
-      
+
    elif  ( numWords >= 3 and words[numWords-3] == "r"):
       #print('now here')
-      # Given the input 'hi there r hi ho', will replace it with 'ho there'  
+      # Given the input 'hi there r hi ho', will replace it with 'ho there'
       fr = words[numWords-2]
       to = words[numWords-1]
       for i in range(len(words)):
@@ -116,23 +121,23 @@ def expandCmdLine(cmdLine, cursor):
       cmdLine = " ".join(words) + after
       cmdLine = re.sub('\s*$', '', cmdLine)
       cursor  = len(cmdLine)
-      
+
    elif ( numWords == 1 and words[0] == "pl" ) or \
           ( numWords >= 2 and words[numWords-2] != "a" and words[numWords-1] == "pl" ):
       # Expand the string "pl"
       words[numWords-1] = "perl -pi -e \"s###g\""
       cmdLine = " ".join(words) + after
       cursor  = cursor + 13
-      
+
    elif ( numWords >= 2 and words[numWords-2] == "for" ):
       # Expand "for var" into a full for loop.
       var = words[numWords-1]
       words[numWords-2] = "for ((" + var + " = ; " + var + " < ; " + var + "++)); " + \
-                          "do ; done" 
+                          "do ; done"
       words[numWords-1] = ""
       cmdLine = " ".join(words) + after
       cursor  = cursor + 5
-      
+
    elif (numWords == 2 and words[0] == "a"):
       # Expand the current alias
       aliasName = words[1]
@@ -147,9 +152,9 @@ def expandCmdLine(cmdLine, cursor):
       words[numWords-1] = a[-pos].split(" ")[-1]
       cmdLine = " ".join(words) + after
       cursor  = len(cmdLine)
-   
+
    return str(cursor) + sep + cmdLine
-   
+
 # Main program
 
 numArgs = len(sys.argv)
@@ -162,4 +167,3 @@ cursor  = int(sys.argv[2])
 sep     = '__sep__'
 
 print expandCmdLine(cmdLine, cursor)
-
