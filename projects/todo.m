7e10d74a77cb60ca5100c1e9e052de0a9034b0ad
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-06-11 10:51:09 -0700

    Update todo

--- projects/todo.m
+++ projects/todo.m
@@ -1,3 +1,28 @@
+CSM work
+
+ - Made the cameras be loaded and saved in parallel in bundle adjustment. This sped up loading by a factor of 4.
+ 
+ - Produced a new calibration for Kaguya Terrain Camera (wide and narrow images)
+   that do not change the focal length. Validated that it does just as well as
+   the previous calibration that did change the focal length.
+ 
+ - Worked with USGS staff at outlier rejection for their bundle adjustment solution
+   with a sparse solver.
+   
+  - Worked on evaluation of the CTX stereo DEMs to MOLA. 
+        
+Verbosity switch for ASP (including progress bar). Will adjust internally the wvrc.
+
+Must add this to the accomplishments:
+
+Made sat_sim create a rig with specified offsets between the sensors in the sensor plane and between footprints on the ground (at nadir). The resulting orientations are computed automatically. This rig will then be used to produce all the cameras and images.
+
+
+Must support distortion in sat_sim and in jitter solve with frame cameras.
+
+Must throw an error!
+--num-matches-from-disparity does not work with --left-image-crop-win
+
 STV: 
 
 Performed a large-scale experiment with 1000 SkySat images (3 rigs with 3 sensors each). Validated that accpetable results were produced. Found out that merely adding more datasets does not improve the absolute geolocation accuracy, as each of these datasets has its own systematic bias, which does not get averaged away. Additional alignment to the ground is still necessary after a self-consistent solution is produced.
@@ -6,13 +31,35 @@ Made bundle_adjust export CSM Frame camera information to the NVM format used by
 
 Ensured that the rig_calibrator, ASP, and CSM are in full agreement when it comes to the radial-tangential lens distortion model and that conversions are handled properly. All these are separate implementations so this needed some work.
 
-Validated the bundle_adjust versatility on a collection of aerial images with very strong distortion, using our internal ratio of polynomials distortion model (this not a usual model in the computer vision community). The results were very good.
+Validated the bundle_adjust versatility on a collection of aerial images with very strong distortion, using our internal RPC (ratio of polynomials) distortion model (this not a usual model in the computer vision community). The results were very good.
  
 Many improvements to the rig calibration software to have it handle orbital images.
-Addressed issues: naming convention, ground counstraints, filtering out outliers consistent with bundle_adjust, constrain rotation and/or translation of rig transforms.
+Addressed issues: ground counstraints, filtering out outliers consistent with bundle_adjust, constrain rotation and/or translation of rig transforms.
 
-Added the ability to create simulated images as if produced with a frame camera rig.
-// https://stereopipeline.readthedocs.io/en/latest/tools/sat_sim.html#sat-sim-rig
+Validated on a large SkySat set (900 images, split among 3 sensors and 3 perspectives for each) that these can be fit on a rig using our rig calibrator program. SkySat does not assume all images on the rig are acquired simultaneously, so this was a good test for this feature of our software.
+
+Added the ability to create simulated images as if produced with a rig.
+The rig can have a mix of linescan and frame cameras.
+// https://stereopipeline.readthedocs.io/en/latest/tools/sat_sim.html#modeling-a-rig
+
+Added ability to model acquistion time along the oribit in the satellite simulator,
+for frame and linescan img ages. Creation time is saved as part of frame camera
+file name and in the metadata of the linescan images.
+// https://stereopipeline.readthedocs.io/en/latest/tools/sat_sim.html#modeling-time
+
+Found a problem in the CSM model implementation when it comes to handling time. A fix
+was accepted by the upstream project.
+
+Added the ability to create camera files given longitude, latitude, height, and roll, pitch, yaw. This is useful for creating cameras based on measurments from an aircraft.
+// https://stereopipeline.readthedocs.io/en/latest/tools/cam_gen.html#cam-gen-extrinsics
+
+Moved rig_calibrator to the ASP repository (it was originally a codebase in the context of indoor robotics). This needed refactoring and integration work.
+
+Added to the jtiter solver the ability to model a set of rigs, with each rig having a linescan sensor and which can also have other combinations of linescan and frame sensors. This was a significant amount of new software.
+
+Validated in a simple experiment that a rig setup improves the control of yaw, as compared to sensors not being tied by a rig constraint. Will research this more going forward.
+
+Added a tool that can greatly help solve  for lens distortion that manifests itself as large horizontal warping in the  DEM. https://stereopipeline.readthedocs.io/en/latest/tools/dem2gcp.html#dem2gcp 
 
 MPR
 
@@ -30,6 +77,9 @@ Developed a process for creating terrain models from VIPER data acquired with
 the forward and aft camera in the sandbox. Validated with ground truth.
 This required additional functionality for the righ calibration
 and bundle adjustment programs. 
+
+// Must support u, v, w throughout the codebase, especially when initializing
+// cameras with GCP in cam_gen and bundle_adjust.
 
 parallel_stereo --correlator-mode does not clean the subdirs!
 
@@ -60,7 +110,7 @@ all the points to be meshed. This will ensure several consistent meshes.
 
 point2dem: Make local projection the default.
 
-Old
+Old STV work. See below for old CSM work.
 
 Added the ability to control, per each group of cameras sharing intrinsics,
 which intrinsics should be optimized, independent of other groups. This
