830eefca3fa11d374bdfe2f487e7a54156f2eeb5
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-09-24 17:51:32 -0700

    Add filter stuff

---
+++ bin/filter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Read all lines from standard input. Print only those lines for which column
+# col is at least equal to val. The first column is column 1. 
+
+import sys
+
+# Ensure that the correct number of arguments are provided
+if len(sys.argv) != 3:
+    print("Usage: filter.py col val")
+    sys.exit(1)
+  
+# Column value
+col = int(sys.argv[1])
+# Value to compare
+val = float(sys.argv[2])
+
+# Read the lines from standard input. Iterate over them.
+for line in sys.stdin:
+    # Split the line into columns
+    columns = line.split()
+    
+    # if col - 1 is negative, add the number of columns to it
+    if col - 1 < 0:
+        col = col + len(columns)
+        
+    # If the column is at least equal to the value, print the line
+    if float(columns[col - 1]) >= val:
+        print(line, end="")
+
+
