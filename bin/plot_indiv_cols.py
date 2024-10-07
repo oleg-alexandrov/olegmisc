3360f642f73acb62009c839643403dd1cb4e4047
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-10-07 12:45:44 -0700

    Add 3rd column for bin/plot_indiv_cols.py

--- bin/plot_indiv_cols.py
+++ bin/plot_indiv_cols.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
-# Plot indices vs each column
+# Plot indices vs each column. Columns start from 1. Assume 2 or 3 columns.
+
+# Restrict the range. This last things should be an option, such as -min -1 -max
+# 3.
 
 import sys, os, re
 import matplotlib.pyplot as plt
@@ -12,12 +15,13 @@ if len(sys.argv) < 4:
 filename = sys.argv[1]
 col1 = int(sys.argv[2])
 col2 = int(sys.argv[3])
+col3 = -1
 
-style = '.'
+# If there is another argument, it is the third column
 if len(sys.argv) > 4:
-    style = sys.argv[4]
+    col3 = int(sys.argv[4])
 
-X, Y = [], []
+X, Y, Z = [], [], []
 for line in open(filename, 'r'):
     count = 0
     for s in line.split():
@@ -25,13 +29,23 @@ for line in open(filename, 'r'):
             X.append(float(s))
         if count == col2 - 1:
             Y.append(float(s))
+        
+        # See if col3 is present, and then create Z
+        if col3 != -1 and count == col3 - 1:
+            Z.append(float(s))
+            
         count += 1
 
 # Let I be just the indices from 0 to len(X) - 1
 I = range(len(X))
-plt.plot(I, X, 'b')
-plt.plot(I, Y, 'r')
+plt.plot(I, X, 'b.')
+plt.plot(I, Y, 'r.')
+
+# If there is Z, and is not empty, plot it in green
+if col3 != -1 and len(Z) > 0:
+    plt.plot(I, Z, 'g.')
+    
+# Restrict the y axis range from -1 to 3
+plt.ylim(-1, 3)
 
 plt.show()
-
-
