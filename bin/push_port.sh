6131261bac8db5195f534b3a4e3729c48e42ac0b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2025-12-14 20:26:33 -0800

    Save one ssh call with port pushing

--- bin/push_port.sh
+++ bin/push_port.sh
@@ -1,42 +1,54 @@
 #!/bin/bash
 
-# Set up reverse port forwarding and record that part on the remote machine.
+# Create a reverse SSH tunnel to the given machine, trying ports from 5001 to
+# 5999. Push the selected port to the remote machine using bin/set_port.sh so
+# that it can be used for the reverse SSH connection.
 
 if [ "$#" -lt 1 ]; then echo Usage: $0 machine; exit; fi
 machine=$1; shift
 
-# Read the last port used from a file
+# Socket file for multiplexing
+sock="/tmp/ssh_socket_${machine}_$$"
+
 portFile=/tmp/port_${machine}.txt
-if [ ! -f $portFile ]; then
-    touch $portFile
-fi
+[ ! -f $portFile ] && touch $portFile
 
-# Read the port. Keep it in boudnds.
 port=$(cat $portFile)
-if [ "$port" == "" ] || [ "$port" -ge 9999 ]; then
-    port=5000 
-fi
+if [ -z "$port" ] || [ "$port" -ge 9999 ]; then port=5000; fi
+((port++))
 
-# Increment it
-((port++));
+# Clean up socket on exit
+trap "rm -f $sock" EXIT
 
-ans=1
-while [ 1 ]; do
-    echo Trying port $port
-    ssh $machine -N -X -f -R ${port}:localhost:22
-    # Updating the remote machine's port requires a second ssh command
-    ssh $machine "bin/set_port.sh $port"
-    ans=$?
-    if [ "$ans" -eq "0" ]; then
-      echo Success
-      # Save it for next time
-      echo $port > $portFile
-      break
-    fi
+while true; do
+    echo "Trying port $port"
+
+    # 1. Start maser connection with remote port forwarding
+    # -M: Master mode
+    # -S: Socket path
+    # -o ExitOnForwardFailure=yes: Fails immediately if port is busy (saves time).
+    # Then the loop will try the next port.
+    ssh -o ExitOnForwardFailure=yes -M -S $sock -f -N -R ${port}:localhost:22 $machine
     
-    ((port=port+1))
+    if [ $? -eq 0 ]; then
+        # 2. Run the check using the existing socket (Instant)
+        ssh -S $sock $machine "bin/set_port.sh $port"
+        
+        if [ $? -eq 0 ]; then
+            echo "Success"
+            echo $port > $portFile
+            break
+        else
+            # If set_port.sh failed, close the tunnel we just opened
+            ssh -S $sock -O exit $machine
+        fi
+    fi
+
+    ((port++))
     if [ "$port" -ge 5999 ]; then port=5000; fi
 done
 
-echo Updating the local machine time
+# Update the local time as otherwise compilation may fail due to time skew
+echo "Updating local time"
 sudo ntpdate time.google.com
+
